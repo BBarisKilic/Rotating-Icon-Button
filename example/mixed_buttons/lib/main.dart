8e0067095c3c7e62b171c9e2801e6fc085d408fe
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-05-28 22:53:37 +0300

    Completed mixed_buttons.

--- example/mixed_buttons/lib/main.dart
+++ example/mixed_buttons/lib/main.dart
@@ -1,115 +1,254 @@
+// Copyright 2022 The RotatingIconButton Author. All rights reserved.
+// Use of this source code is governed by a MIT-style license that
+// can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+import 'package:rotating_icon_button/rotating_icon_button.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ExampleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Mixed Buttons',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+        home: const MixedButtons(),
+      );
+}
+
+class MixedButtons extends StatefulWidget {
+  const MixedButtons({Key? key}) : super(key: key);
+
+  @override
+  State<MixedButtons> createState() => _MixedButtonsState();
+}
+
+class _MixedButtonsState extends State<MixedButtons> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Colors.cyanAccent,
+        appBar: AppBar(
+          title: const Text(
+            'Mixed Buttons',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.cyanAccent,
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              buildFirstRow(),
+              buildSecondRow(),
+              buildThirdRow(),
+              buildFourthRow(),
+              buildFifthdRow(),
+              buildSixthRow(),
+            ],
+          ),
+        ),
+      );
+
+  Widget buildFirstRow() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          RotatingIconButton(
+            onTap: () {},
+            background: Colors.transparent,
+            clockwise: false,
+            child: const Icon(
+              Icons.settings,
+              color: Colors.black,
+              size: 30,
+            ),
+          ),
+          RotatingIconButton(
+            onTap: () {},
+            background: Colors.transparent,
+            padding: const EdgeInsets.all(2.0),
+            shape: ButtonShape.circle,
+            child: const Icon(
+              Icons.settings_outlined,
+              color: Colors.red,
+              size: 30,
+            ),
+          ),
+        ],
+      );
+
+  Row buildSecondRow() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          RotatingIconButton(
+            onTap: () {},
+            elevation: 10.0,
+            background: Colors.amber,
+            clockwise: false,
+            child: const Icon(
+              Icons.search,
+              color: Colors.indigo,
+            ),
+          ),
+          RotatingIconButton(
+            onTap: () {},
+            elevation: 10.0,
+            background: Colors.indigo,
+            shape: ButtonShape.circle,
+            child: const Icon(
+              Icons.search,
+              color: Colors.amber,
+            ),
+          ),
+        ],
+      );
+
+  Row buildThirdRow() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          RotatingIconButton(
+            onTap: () {},
+            elevation: 20.0,
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 40,
+            ),
+            rotateType: RotateType.semi,
+            background: Colors.deepPurple,
+            clockwise: false,
+            child: const Icon(
+              Icons.attach_file,
+              color: Colors.amberAccent,
+            ),
+          ),
+          RotatingIconButton(
+            onTap: () {},
+            elevation: 20.0,
+            background: Colors.amberAccent,
+            shape: ButtonShape.circle,
+            rotateType: RotateType.semi,
+            padding: const EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 20,
+            ),
+            child: const Icon(
+              Icons.attach_file,
+              color: Colors.deepPurple,
+            ),
+          ),
+        ],
+      );
+
+  Row buildFourthRow() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          RotatingIconButton(
+            onTap: () {},
+            elevation: 10.0,
+            shadowColor: Colors.pink,
+            borderRadius: 20.0,
+            rotateType: RotateType.semi,
+            clockwise: false,
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 40,
+            ),
+            background: Colors.grey,
+            child: const Icon(
+              Icons.navigation,
+              color: Colors.pink,
+            ),
+          ),
+          RotatingIconButton(
+            onTap: () {},
+            elevation: 10.0,
+            shadowColor: Colors.pink,
+            background: Colors.pink,
+            rotateType: RotateType.semi,
+            shape: ButtonShape.circle,
+            padding: const EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 20,
+            ),
+            child: const Icon(
+              Icons.navigation,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      );
+
+  Row buildFifthdRow() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          RotatingIconButton(
+            onTap: () {},
+            elevation: 10.0,
+            borderRadius: 10.0,
+            rotateType: RotateType.full,
+            duration: const Duration(milliseconds: 600),
+            background: Colors.amber,
+            clockwise: false,
+            child: const Icon(
+              Icons.rotate_left,
+              color: Colors.indigo,
+            ),
+          ),
+          RotatingIconButton(
+            onTap: () {},
+            elevation: 10.0,
+            rotateType: RotateType.full,
+            duration: const Duration(milliseconds: 600),
+            background: Colors.indigo,
+            shape: ButtonShape.circle,
+            child: const Icon(
+              Icons.rotate_right,
+              color: Colors.amber,
+            ),
+          ),
+        ],
+      );
+
+  Widget buildSixthRow() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            color: Colors.brown,
+            padding: const EdgeInsets.all(2.0),
+            child: RotatingIconButton(
+              onTap: () {},
+              background: Colors.cyanAccent,
+              padding: EdgeInsets.zero,
+              clockwise: false,
+              child: const Icon(
+                Icons.add,
+                color: Colors.brown,
+              ),
+            ),
+          ),
+          RotatingIconButton(
+            onTap: () {},
+            background: Colors.deepOrange,
+            padding: const EdgeInsets.all(2.0),
+            shape: ButtonShape.circle,
+            child: Container(
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.cyanAccent,
+              ),
+              child: const Icon(
+                Icons.add,
+                color: Colors.deepOrange,
+              ),
+            ),
+          ),
+        ],
+      );
 }
